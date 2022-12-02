023c095300d5777a19cb8a89fc92b4eb011f5bcc
Moulali <vempalli.moulali@capgemini.com>
2022-12-02 12:33:51 +0530

    first commit

---
+++ OnlineMedicalShopProject/src/test/java/com/OnlineMedicineShoppingSystem/OnlineMedicineShoppingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineMedicineShoppingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMedicineShoppingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
